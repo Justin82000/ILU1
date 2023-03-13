676439271e711145417481ebb0fda272e2ca2afe
Justin <125344973+Justin82000@users.noreply.github.com>
2023-03-13 19:14:26 +0100

    Update Gaulois.java

--- personnages/src/personnages/Gaulois.java
+++ personnages/src/personnages/Gaulois.java
@@ -19,11 +19,17 @@ public class Gaulois {
 		return "Le gaulois " + nom + " : ";
 }
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());romain.recevoirCoup(force / 3);
 }
-public static void main(String[] args) {
-		 asterix (nom = "Astérix", force = 8),
-		 System.out.println(asterix);
-//TODO créer un main permettant de tester la classe Gaulois
+	
+	@Override
+	public String toString() {	
+	return "Gaulois [nom=" + nom + ", force=" + force
+	+ ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix",8);
+		System.out.println(asterix);
 }
 }
