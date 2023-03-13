481232a0f33b64ace08eb4872d971a57cd520da7
Justin <125344973+Justin82000@users.noreply.github.com>
2023-03-13 22:33:35 +0100

    update TP1
    fin du tp1

--- personnages/src/histoire/Scenario.java
+++ personnages/src/histoire/Scenario.java
@@ -1,5 +1,6 @@
 package histoire;
 
+import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
 
@@ -7,11 +8,25 @@ public class Scenario {
 
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Asterix",8);
+		Gaulois obelix = new Gaulois("Obelix",15);
 		Romain minus = new Romain("Minus",6);
-		asterix.parler("Bonjour a tous");
+		Druide panoramix = new Druide("Panoramix",5,10);
+		/*
+		 * asterix.parler("Bonjour a tous");
+		 * minus.parler("UN GAU... UN GAUGAU...");
+		 * asterix.frapper(minus);
+		 * asterix.frapper(minus); 
+		 * asterix.frapper(minus);
+		 */
+		panoramix.parler("« Bonjour, je suis le druide Panoramix "
+				+ "et ma potion peut aller d'une force 5 à 10");
+		panoramix.parler("« Je vais aller préparer une petite potion..");
+		panoramix.preparerPotion();
+		panoramix.booster(obelix);
+		obelix.parler("« Par Bélénos, ce n'est pas juste !");
+		asterix.boirePotion(6);
+		asterix.parler("Bonjour");
 		minus.parler("UN GAU... UN GAUGAU...");
-		asterix.frapper(minus);
-		asterix.frapper(minus);
 		asterix.frapper(minus);
 	}
 
--- personnages/src/personnages/Gaulois.java
+++ personnages/src/personnages/Gaulois.java
@@ -4,6 +4,7 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private int forcePotion = 1;
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -20,9 +21,14 @@ public class Gaulois {
 }
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3)*effetPotion);
 }
-	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		if (forcePotion==3) {
+			System.out.println(" Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée");
+		}
+	}
 	@Override
 	public String toString() {	
 	return "Gaulois [nom=" + nom + ", force=" + force
@@ -35,5 +41,6 @@ public class Gaulois {
 		System.out.println(asterix);
 		System.out.println(asterix.prendreParole());
 		asterix.frapper(patrick);
+		asterix.boirePotion(3);
 	}
 }
