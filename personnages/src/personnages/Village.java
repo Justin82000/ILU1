dc60b93b6e0545a2bfcfff3bf4eaf75276f54072
Justin <125344973+Justin82000@users.noreply.github.com>
2023-03-14 11:00:19 +0100

    update

--- personnages/src/personnages/Village.java
+++ personnages/src/personnages/Village.java
@@ -3,17 +3,49 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private int nbVillageois = 0;
+	private int nbVillageoisMax;
+	private Gaulois villageois[];
 	
-	public Village(String nom, Chef chef) {
+	public Village(String nom,int nbVillageoisMax) {
 		this.nom = nom;
-		this.chef = chef;
+		this.villageois = new Gaulois[nbVillageoisMax];	
+	}	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
 	}
-	
+	public Gaulois trouverHabitant(int num) {
+		return villageois[num];
+	}
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef "+chef.getNom()
+		+" vivent les legendaires gaulois :");
+		for (int i=0;i<nbVillageois;i++) {
+			System.out.println("- "+villageois[i].getNom());
+		}
+	}
 	public String getNom() {
 		return nom;
 	}
 	
 	public void setChef(Chef chef) {
 		this.chef=chef;
+	}
+	public static void main(String[] args) {
+		
+		 Village village = new Village("Village des Irreductible", 30);
+			/* village.trouverHabitant(30); */
+		Chef abraracourcix = new Chef("Abraracourcix",6,village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Asterix",8);
+		village.ajouterHabitant(asterix);
+		/*
+		 * village.trouverHabitant(1); System.out.println(asterix); Affiche le gaulois
+		 * present a l'indice 1 ainsi que ces caracteristique !
+		 */
+		Gaulois obelix = new Gaulois("Obelix",25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 }
